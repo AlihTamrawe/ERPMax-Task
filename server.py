117b9d2242a6794181d1a1cf8a780bff3f4740fd
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2023-05-14 18:31:16 +0300

    double

--- server.py
+++ server.py
@@ -2,12 +2,15 @@ from operator import index
 from flask import Flask, render_template, request, redirect  
 from  mysqlconnection import connectToMySQL
 from datetime import datetime  
+import itertools 
 
 
 #----------------------------1-halloworld
 f=100
 app = Flask(__name__)  
-
+@app.context_processor
+def add_imports():
+    return dict( itertools=itertools )
 @app.route("/one")     #test
 def index():
     mysql = connectToMySQL('inventory_management')	        
@@ -126,11 +129,21 @@ def add_move_to_db():
 @app.route('/movement') # update move form
 def show_movement():
     mysql = connectToMySQL('inventory_management')	        
-    query = "SELECT * FROM productmovement JOIN product JOIN location where product_id_m  = product_id  or location_id = to_location and location_id = from_location  ;"
+    query = "SELECT * FROM productmovement     INNER JOIN product   ON     product_id_m = product_id  ;"
     result = mysql.query_db(query)   
+
+    mysql2 = connectToMySQL('inventory_management')	        
+    query2 = "SELECT * FROM productmovement     INNER JOIN location  ON     idlocation = to_location  "
+    result2 = mysql2.query_db(query2)   
+
+
+    mysql3 = connectToMySQL('inventory_management')	        
+    query3 = "SELECT * FROM productmovement     INNER JOIN location  ON     idlocation = from_location ;"
+    result3 = mysql3.query_db(query3)   
     print(result)
     f=11
-    return render_template("index.html",all_movement = result,flag =f) 
+
+    return render_template("index.html",all_movement =result2 , all_product=result,all_location = result3 ,flag =f) 
 
 @app.route('/updatelocation/<id>') # update location form
 def update_location_form(id):
