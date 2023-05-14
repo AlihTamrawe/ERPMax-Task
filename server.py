1d2a8afea53b759ff3a54b7d69b4cf5bb37eb00c
AlihTamrawe <59147506+AlihTamrawe@users.noreply.github.com>
2023-05-14 15:33:18 +0300

    Movement page and backend detailes

--- server.py
+++ server.py
@@ -2,32 +2,57 @@ from operator import index
 from flask import Flask, render_template, request, redirect  
 from  mysqlconnection import connectToMySQL
 #----------------------------1-halloworld
-f=0
-app = Flask(__name__)   
-@app.route("/one")
+f=100
+app = Flask(__name__)  
+
+@app.route("/one")     #test
 def index():
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
-    product = mysql.query_db('SELECT * FROM product;')  # call the query_db function, pass in the query as a string
+    mysql = connectToMySQL('inventory_management')	        
+    product = mysql.query_db('SELECT * FROM product;')  
     print(product)
     f=1
     return render_template("index.html",all_products = product,flag =f)
 
+
+
+
+@app.route("/move") 
+def index_move():
+    mysql = connectToMySQL('inventory_management')	        
+    product = mysql.query_db('SELECT * FROM product;')  
+    print(product)
+    mysql2 = connectToMySQL('inventory_management')	        
+    locations = mysql2.query_db('SELECT * FROM location;')  
+    f=0
+    return render_template("index.html",all_products = product,all_locations = locations,flag =f)
+
+
+@app.route("/report") 
+def index_report():
+    mysql = connectToMySQL('inventory_management')	        
+    product = mysql.query_db('SELECT * FROM product;')  
+    print(product)
+    mysql2 = connectToMySQL('inventory_management')	        
+    locations = mysql2.query_db('SELECT * FROM location;')  
+    f=10
+    return render_template("index.html",all_products = product,all_locations = locations,flag =f)
+
 @app.route("/two")
 def index3():
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
-    locations = mysql.query_db('SELECT * FROM location;')  # call the query_db function, pass in the query as a string
+    mysql = connectToMySQL('inventory_management')	        
+    locations = mysql.query_db('SELECT * FROM location;')  
     print(locations)
     f=3
     return render_template("index.html",all_locations = locations,flag =f)
 
 @app.route("/")
 def index2():
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
-    products = mysql.query_db('SELECT * FROM product;')  # call the query_db function, pass in the query as a string
-    mysql2 = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
+    mysql = connectToMySQL('inventory_management')	        
+    products = mysql.query_db('SELECT * FROM product;')  
+    mysql2 = connectToMySQL('inventory_management')	        
     locations = mysql2.query_db('SELECT * FROM location JOIN product where location_id = idlocation ;')  
-    mysql3 = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
-    allocations = mysql3.query_db('SELECT * FROM location  ;')  # call the query_db function, pass in the query as a string
+    mysql3 = connectToMySQL('inventory_management')	        
+    allocations = mysql3.query_db('SELECT * FROM location  ;')  
 
     print(locations)
     f=2
@@ -38,7 +63,7 @@ def add_product_to_db():
     is_valid = True		# assume True
     if len(request.form['pname']) < 4:
         is_valid = False
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
+    mysql = connectToMySQL('inventory_management')	        
     QUERY = 'INSERT INTO inventory_management.product (product_name , location_id ) values (%(p_name)s,%(loca_id)s );'
     data = {
         "p_name": request.form["pname"],
@@ -58,7 +83,7 @@ def add_product_to_db():
 
 @app.route("/create_location", methods=["POST"])  # Create location form 
 def add_location_to_db():
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
+    mysql = connectToMySQL('inventory_management')	        
     QUERY = 'INSERT INTO inventory_management.location (location_name  ) values (%(l_name)s );'
     data = {
          "l_name": request.form["lname"]
@@ -71,7 +96,7 @@ def add_location_to_db():
 
 @app.route('/updatelocation/<id>') # update location form
 def update_location_form(id):
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
+    mysql = connectToMySQL('inventory_management')	        
     query = "SELECT * FROM location WHERE idlocation = %(id)s;"
     data = { 'id' : id }
     result = mysql.query_db(query, data)    
@@ -83,7 +108,7 @@ def update_location_form(id):
 
 @app.route("/update_location/<id>", methods=["POST"]) # update location Route
 def edit_location_to_db(id):
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
+    mysql = connectToMySQL('inventory_management')	        
     QUERY = ' UPDATE location SET location_name = %(l_name)s WHERE idlocation = %(id)s ;'
     data = {
          "l_name": request.form["lname"],
@@ -99,7 +124,7 @@ def edit_location_to_db(id):
 
 @app.route('/updateproduct/<id>') # update prod form
 def update_product_form(id):
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
+    mysql = connectToMySQL('inventory_management')	        
     query = "SELECT * FROM product WHERE product_id = %(id)s;"
     data = { 'id' : id }
     result = mysql.query_db(query, data)    
@@ -111,7 +136,7 @@ def update_product_form(id):
 
 @app.route("/update_product/<id>", methods=["POST"]) # update prod Route
 def edit_product_to_db(id):
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
+    mysql = connectToMySQL('inventory_management')	        
     QUERY = ' UPDATE product SET product_name = %(p_name)s WHERE product_id = %(id)s ;'
     data = {
          "p_name": request.form["pname"],
@@ -124,7 +149,7 @@ def edit_product_to_db(id):
 
 @app.route("/delete_product/<id>") # DELETE prod Route
 def delete_product_to_db(id):
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
+    mysql = connectToMySQL('inventory_management')	        
     QUERY = 'DELETE FROM product WHERE product_id = %(id)s  ;'
     data = {
         "id" : id
@@ -139,7 +164,7 @@ def delete_product_to_db(id):
 
 @app.route("/delete_location/<id>") # DELETE location Route
 def delete_location_to_db(id):
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
+    mysql = connectToMySQL('inventory_management')	        
     QUERY = 'DELETE FROM location WHERE idlocation = %(id)s  ;'
     data = {
         "id" : id
@@ -154,7 +179,7 @@ def delete_location_to_db(id):
 
 @app.route('/product/<id>') # Show prod
 def show_product_profile(id):
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
+    mysql = connectToMySQL('inventory_management')	        
     query = "SELECT * FROM product WHERE product_id = %(id)s;"
    
     data = { 'id' : id }
@@ -162,7 +187,7 @@ def show_product_profile(id):
     result = mysql.query_db(query, data)    
     print(result[0] )
 
-    mysql = connectToMySQL('inventory_management')	        # call the function, passing in the name of our db
+    mysql = connectToMySQL('inventory_management')	        
     query2 = "SELECT * FROM location WHERE idlocation = %(id2)s;"
     data1 = { 'id2' : result[0]["Location_id"] }
     result2 = mysql.query_db(query2, data1)    
